1a3e647b6a22811f29b62345359af36cd9ec4a5d
rijkerd <rich2buzy@hotmail.com>
2018-11-12 09:50:52 +0300

    production deployment test

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ app.mount('./routes', './routes/users');
 //start app
 app.start(function onStart(error, env) {
     console.log(env.PORT);
+    console.log(env.NODE_ENV);
     console.log(`visit http://0.0.0.0:${env.PORT}/v1/`);
 });
