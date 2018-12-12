5256555892e3063332c84303d22e9ec18f6e1d47
rijkerd <rich2buzy@hotmail.com>
2018-12-12 11:23:51 +0300

    -Add documentation of the api -Add users list fetch -Destructure the request.body -Change response to String with name

--- controllers/user.js
+++ controllers/user.js
@@ -8,27 +8,45 @@ const User = require('../models/index');
  * @param response{Object}
  * 
  * @description user controllers function that returns a list of all users
- * @todo Query from the database a list of all available users
  * 
+ * @returns {error} fail to get the list of users
+ * @returns {success} users with name and email fields 
  */
 exports.list = function (request, response) {
-    const user = User.fake(5);
-    response.json(user);
+    User.find({}, 'name email', (error, users) => {
+        if (error) {
+            response.status(204).json({ error: 'Fetching users' });
+        }
+        response.status(200).json(users);
+    });
 };
 
 
+/**
+ * @method register
+ * @param request{Object}
+ * @param response{Object}
+ * 
+ * @description user controller function to register user
+ * 
+ * @returns {error} corresponding registration error
+ * @returns {success} message with the created user name
+ */
 exports.register = function (request, response) {
-    console.log(request.body);
+    const { email, name, password } = response.body;
+
     const credentails = {
-        email: request.body.email,
-        name: request.body.name,
-        password: request.body.password
-    }
+        email,
+        name,
+        password
+    };
+
     User.register(credentails, function (error, registerable) {
         if (error) {
             response.status(400).json(error);
         }
-        response.status(201).json(registerable);
+        const { name } = registerable;
+        response.status(201).json({ message: `User ${name} created` });
     });
 };
 
