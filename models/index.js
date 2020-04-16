4726e1459f21bb9f474459913a0daeb1685fa8dd
rijkerd <richardaggrey7@gmail.com>
2020-04-16 22:34:50 +0300

    refactor: implement model action using rest-actions

--- models/index.js
+++ models/index.js
@@ -5,44 +5,45 @@ const irina = require('irina');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        lowercase: true,
-        trim: true,
-        required: [true, 'Email field required'],
-        fake: {
-            generator: 'internet',
-            type: 'email'
-        }
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    required: [true, 'Email field required'],
+    fake: {
+      generator: 'internet',
+      type: 'email',
     },
+  },
 
-    name: {
-        type: String,
-        required: [true, 'Name is required'],
-        unique: true,
-        trim: true,
-        fake: {
-            generator: 'internet',
-            type: 'userName'
-        }
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true,
+    trim: true,
+    fake: {
+      generator: 'internet',
+      type: 'userName',
     },
+  },
 
-    profileImage: {
-        type: String,
-        fake: {
-            generator: 'image',
-            type: 'avatar'
-        }
+  profileImage: {
+    type: String,
+    fake: {
+      generator: 'image',
+      type: 'avatar',
     },
+  },
 
-    password: {
-        type: String,
-        unique: true,
-        required: [true, 'Please Provide a password']
-    }
+  password: {
+    type: String,
+    unique: true,
+    required: [true, 'Please Provide a password'],
+  },
 });
 
 mongoose.plugin(require('@lykmapipo/mongoose-faker'));
+mongoose.plugin(require('mongoose-rest-actions'));
 userSchema.plugin(irina);
 userSchema.plugin(timestamp);
 
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const controller = require('../../controllers/user');
-
 const { Router } = require('@lykmapipo/express-router-extra');
 const { getString } = require('@lykmapipo/env');
+const User = require('../../models/');
 
 const NODE_ENV = getString('NODE_ENV', 'development');
 
@@ -18,8 +17,34 @@ router.get('/.:ext?', function (request, response) {
   });
 });
 
-router.get('/users', controller.list);
+router.get('/users', (req, res, next) => {
+  const options = req.options;
+  // const users = User.fake(50);
 
-router.post('/register', controller.register);
+  User.find(options, (error, results) => {
+    if (error) {
+      return res.status(400).json(error);
+    }
+    res.status(200).json(results);
+  });
+});
+
+router.post('/register', (req, res, next) => {
+  const { email, name, password } = req.body;
+
+  const credentails = {
+    email,
+    name,
+    password,
+  };
+
+  User.register(credentails, function (error, registerable) {
+    if (error) {
+      res.status(400).json(error);
+    }
+    // console.log(registerable);
+    res.status(201).json({ message: `User ${name} created` });
+  });
+});
 
 module.exports = router;
