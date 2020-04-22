a1e2857c975cd49615ea070848789e4381424def
rijkerd <richardaggrey7@gmail.com>
2020-04-22 11:28:21 +0300

    refactor: implement to user model to express actions

---
+++ User/user.http.router.js
@@ -0,0 +1,155 @@
+const _ = require('lodash');
+const { getString } = require('@lykmapipo/env');
+const { include } = require('@lykmapipo/include');
+const { Router } = require('@lykmapipo/express-common');
+const {
+  getFor,
+  schemaFor,
+  getByIdFor,
+  postFor,
+  patchFor,
+  deleteFor,
+} = require('@lykmapipo/express-rest-actions');
+
+/* constants */
+const API_VERSION = getString('API_VERSION', '1.0.0');
+const PATH_SINGLE = '/users/:id';
+const PATH_LIST = '/users';
+// const PATH_EXPORT = '/users/export';
+const PATH_SCHEMA = '/users/schema/';
+
+const User = include(__dirname, 'user.model');
+
+const router = new Router({
+  version: API_VERSION,
+});
+
+/**
+ * @api {get} /users List Users
+ * @apiVersion 1.0.0
+ * @apiName GetUsers
+ * @apiGroup User
+ * @apiDescription Returns a list of users
+ * @apiUse RequestHeaders
+ * @apiUse Users
+ *
+ * @apiUse RequestHeadersExample
+ * @apiUse UsersSuccessResponse
+ */
+
+router.get(
+  PATH_LIST,
+  getFor({
+    get: (options, done) => User.get(options, done),
+  })
+);
+
+/**
+ * @api {get} /user/schema Get User Schema
+ * @apiVersion 1.0.0
+ * @apiName GetUserSchema
+ * @apiGroup User
+ * @apiDescription Returns user json schema definition
+ * @apiUse RequestHeaders
+ */
+
+router.get(
+  PATH_SCHEMA,
+  schemaFor({
+    getSchema: (query, done) => {
+      const jsonSchema = User.jsonSchema();
+      return done(null, jsonSchema);
+    },
+  })
+);
+
+/**
+ * @api {get} /user/:id Get Existing User
+ * @apiVersion 1.0.0
+ * @apiName GetUser
+ * @apiGroup User
+ * @apiDescription Get existing User
+ * @apiUse RequestHeaders
+ * @apiUse User
+ *
+ * @apiUse RequestHeadersExample
+ * @apiUse UserSuccessResponse
+ */
+
+router.get(
+  PATH_SINGLE,
+  getByIdFor({
+    getById: (options, done) => User.findById(options, done),
+  })
+);
+
+/**
+ * @api {post} /users/register Register new User
+ * @apiVersion 1.0.0
+ * @apiName RegisterUser
+ * @apiGroup User
+ * @apiDescription Register new user
+ * @apiUse RequestHeaders
+ * @apiUse User
+ *
+ * @apiUse RequestHeadersExample
+ * @apiUse PermissionSuccessResponse
+ */
+
+router.post(
+  `${PATH_LIST}/register`,
+  postFor({
+    post: (body, done) => {
+      const options = _.pick(body, ['name', 'email', 'password']);
+      return User.register(options, done);
+    },
+  })
+);
+
+/**
+ * @api {patch} /users/:id Patch Existing User
+ * @apiVersion 1.0.0
+ * @apiName PatchUser
+ * @apiGroup User
+ * @apiDescription Patch existing user
+ * @apiUse RequestHeaders
+ * @apiUse User
+ *
+ * @apiUse RequestHeadersExample
+ * @apiUse UserSuccessResponse
+ */
+
+router.patch(
+  PATH_SINGLE,
+  patchFor({
+    patch: (query, done) => {
+      const options = _.pick(query, '_id', 'name');
+      return User.patch(options, done);
+    },
+  })
+);
+
+/**
+ * @api {delete} /user/:id Delete Existing User
+ * @apiVersion 1.0.0
+ * @apiName DeleteUser
+ * @apiGroup User
+ * @apiDescription Delete existing user
+ * @apiUse RequestHeaders
+ * @apiUse User
+ *
+ * @apiUse RequestHeadersExample
+ * @apiUse UserSuccessResponse
+ */
+
+router.delete(
+  PATH_SINGLE,
+  deleteFor({
+    del: (query, done) => {
+      const options = _.pick(query, '_id');
+      return User.del(options, done);
+    },
+  })
+);
+
+module.exports = router;
---
+++ User/user.model.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const irina = require('irina');
+const actions = require('mongoose-rest-actions');
+
+const { Schema, model } = mongoose;
+
+const SCHEMA_OPTIONS = {
+  id: false,
+  timestamps: false,
+  toJSON: { getters: true },
+  toObject: { getters: true },
+  emitIndexErrors: true,
+};
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: [true, 'Email field required'],
+      fake: {
+        generator: 'internet',
+        type: 'email',
+      },
+    },
+
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      unique: true,
+      trim: true,
+      fake: {
+        generator: 'internet',
+        type: 'userName',
+      },
+    },
+
+    profileImage: {
+      type: String,
+      fake: {
+        generator: 'image',
+        type: 'avatar',
+      },
+    },
+
+    password: {
+      type: String,
+      unique: true,
+      required: [true, 'Please Provide a password'],
+    },
+  },
+  SCHEMA_OPTIONS
+);
+
+UserSchema.pre('validate', function (done) {
+  return this.preValidate(done);
+});
+
+UserSchema.methods.preValidate = function preValidate(done) {
+  // continue
+  return done(null, this);
+};
+
+UserSchema.plugin(actions);
+UserSchema.plugin(irina);
+
+module.exports = exports = model('User', UserSchema);
