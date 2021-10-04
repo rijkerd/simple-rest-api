660898ea6b3709fcc3951a894f4bb03c27eb6dc9
rijkerd <richardaggrey7@gmail.com>
2021-10-04 05:48:52 +0300

    feat: remove graphql

--- index.js
+++ index.js
@@ -6,24 +6,11 @@ const keys = require('lodash/keys');
 const { connect } = require('@lykmapipo/mongoose-common');
 const { getString } = require('@lykmapipo/env');
 const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
-const User = require('./User/user.model');
-const { graphqlHTTP } = require('express-graphql');
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
+// const io = require('socket.io').listen(server);
 
 const MONGODB_URI = getString('MONGODB_URI');
 
 connect(MONGODB_URI, (error) => error);
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-  })
-);
 
 server.listen(app.get('port'), (error) => {
   if (error) {
@@ -35,27 +22,27 @@ server.listen(app.get('port'), (error) => {
   console.log('Worked');
 });
 
-var connectedUser = {};
-io.on('connection', (socket) => {
-  console.log('client connect...', socket.id);
-  console.log('Connected');
+// var connectedUser = {};
+// io.on('connection', (socket) => {
+//   console.log('client connect...', socket.id);
+//   console.log('Connected');
 
-  socket.on('register', (data) => {
-    connectedUser[data] = socket;
-    console.warn('ConnetecdNew', keys(connectedUser));
-  });
+//   socket.on('register', (data) => {
+//     connectedUser[data] = socket;
+//     console.warn('ConnetecdNew', keys(connectedUser));
+//   });
 
-  socket.on('consultation_request', (data) => {
-    const { requester, doctor } = data;
-    connectedUser[requester].emit('new_message', {
-      message: 'This for patient',
-    });
-    connectedUser[doctor].emit('new_message', {
-      message: 'This is for doctor',
-    });
-  });
+//   socket.on('consultation_request', (data) => {
+//     const { requester, doctor } = data;
+//     connectedUser[requester].emit('new_message', {
+//       message: 'This for patient',
+//     });
+//     connectedUser[doctor].emit('new_message', {
+//       message: 'This is for doctor',
+//     });
+//   });
 
-  socket.on('new_message', (data) => {
-    console.log(data);
-  });
-});
+//   socket.on('new_message', (data) => {
+//     console.log(data);
+//   });
+// });
